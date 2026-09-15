bb4420b2cdd29aafe2f290ecc3131124ab413c08
Tiefing Sangaré <tiefingsangare86@gmail.com>
2026-09-15 12:06:12 +0000

    fff

--- app/Services/FcmService.php
+++ app/Services/FcmService.php
@@ -7,6 +7,7 @@ use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
 use Kreait\Firebase\Messaging\AndroidConfig;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 
 class FcmService
 {
